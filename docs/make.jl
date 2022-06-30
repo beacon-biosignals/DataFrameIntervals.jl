394a7c14604ee428b95373a5186bc359a4869591
Phillip Alday <palday@users.noreply.github.com>
2022-06-30 13:26:59 -0500

    CI tweaks and fixes (#6)
    Various yaml/ci fixes

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,16 @@
 using DataFrameIntervals
 using Documenter
 
-DocMeta.setdocmeta!(DataFrameIntervals, :DocTestSetup, :(using DataFrameIntervals);
-                    recursive=true)
-
-makedocs(;
-         modules=[DataFrameIntervals],
+makedocs(; modules=[DataFrameIntervals],
+         authors="Beacon Biosignals, Inc.",
          repo="https://github.com/beacon-biosignals/DataFrameIntervals.jl/blob/{commit}{path}#{line}",
          sitename="DataFrameIntervals.jl",
-         format=Documenter.HTML(;
-                                prettyurls=get(ENV, "CI", "false") == "true",
+         format=Documenter.HTML(; prettyurls=get(ENV, "CI", "false") == "true",
+                                # will be re-directed as needbe for a private repo
+                                canonical="https://beacon-biosignals.github.io/DataFrameIntervals.jl",
                                 assets=String[]),
          pages=["Home" => "index.md"])
+
+deploydocs(; repo="github.com/beacon-biosignals/DataFrameIntervals.jl",
+           devbranch="main",
+           push_preview=true)
